58a73a02de8c95470703a82329fe6a927355048f
Mark Jerde <mark.a.jerde@gmail.com>
2018-02-20 11:28:57 -0600

    Add MJFastImageLoaderView to provide an element that can be placed in Interface Builder for users that desire that pattern.  The plan is to support MJFastImageLoaderNotification child classes that can be pointed at standard UIKit elements as well as child classes of UIKit elements that handle those MJFastImageLoaderNotification child classes internally, so that both pattern are available.  It is possible that MJFastImageLoaderNotification should be a delegate type or that the base MJFastImageLoaderNotification provide everything necessary to ensure notification linking isn't dropped, or that the caller traverse the links themselves.

---
+++ MJFastImageLoader/MJFastImageLoader/MJFastImageLoaderView.swift
@@ -0,0 +1,40 @@
+//
+//  MJFastImageLoaderView.swift
+//  MJFastImageLoader
+//
+//  Created by Mark Jerde on 2/19/18.
+//  Copyright © 2018 Mark Jerde.
+//
+//  This file is part of MJFastImageLoader
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of MJFastImageLoader and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+class MJFastImageLoaderView: UIImageView {
+
+	var imageLoaderUID = -1
+
+	/*func setData(imageData: Data, priority: Int) {
+		imageLoaderUID = MJFastImageLoader.shared.enqueue(image: imageData, priority: priority)
+	}*/
+
+
+}
