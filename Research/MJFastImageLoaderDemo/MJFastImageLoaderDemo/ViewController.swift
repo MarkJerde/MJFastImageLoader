dae0a9cba5c5d1941bb3e186c9011f1a1530b5d3
Mark Jerde <mark.a.jerde@gmail.com>
2018-02-20 21:55:24 -0600

    Detailed demo of MJFastImageLoader displaying six images with controls for various testing.

--- Research/MJFastImageLoaderDemo/MJFastImageLoaderDemo/ViewController.swift
+++ Research/MJFastImageLoaderDemo/MJFastImageLoaderDemo/ViewController.swift
@@ -34,16 +34,88 @@
 import UIKit
 import MJFastImageLoader
 
+// MARK: Sequence Shuffle Extensions
+
+extension MutableCollection where Indices.Iterator.Element == Index {
+	/// Shuffles the contents of this collection.
+	mutating func shuffle() {
+		let c = count
+		guard c > 1 else { return }
+
+		for (firstUnshuffled , unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+			let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+			guard d != 0 else { continue }
+			let i = index(firstUnshuffled, offsetBy: d)
+			self.swapAt(firstUnshuffled, i)
+		}
+	}
+}
+
+extension Sequence {
+	/// Returns an array with the contents of this sequence, shuffled.
+	func shuffled() -> [Iterator.Element] {
+		var result = Array(self)
+		result.shuffle()
+		return result
+	}
+}
+
+// MARK: ViewController
+
 class ViewController: UIViewController {
 
 	@IBOutlet weak var imageView: UIImageView!
-	
+	@IBOutlet weak var imageView2: UIImageView!
+	@IBOutlet weak var imageView3: UIImageView!
+	@IBOutlet weak var imageView4: UIImageView!
+	@IBOutlet weak var imageView5: UIImageView!
+	@IBOutlet weak var imageView6: UIImageView!
+	var imageViews:[UIImageView] = []
+
+	@IBOutlet weak var statusLabel: UILabel!
+
+	@IBOutlet weak var runButton: UIButton!
+	@IBOutlet weak var stopButton: UIButton!
+	@IBOutlet weak var stepButton: UIButton!
+
+	var running = false
+	var enabled = true
+	var imageDatas:[Data] = []
+	var imageUpdaters:[UIImageUpdater?] = [nil,nil,nil,nil,nil,nil]
+	var imageDataIndex = 0
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 
+		//Looks for single or multiple taps.
+		let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+
+		//Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+		//tap.cancelsTouchesInView = false
+
+		view.addGestureRecognizer(tap)
+
+		imageViews = [imageView,
+					  imageView2,
+					  imageView3,
+					  imageView4,
+					  imageView5,
+					  imageView6]
+
+		runButton.isEnabled = false
+		stepButton.isEnabled = false
+		stopButton.isEnabled = false
+
+		MJFastImageLoader.shared.setMinimalCaching( value: true )
 
 		self.startTest()
+	}
+
+	//Calls this function when the tap is recognized.
+	@objc func dismissKeyboard() {
+		//Causes the view (or one of its embedded text fields) to resign the first responder status.
+		view.endEditing(true)
 	}
 
 	let testQueue = DispatchQueue(label: "MJFastImageLoaderDemo.testQueue")
@@ -54,8 +126,9 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-	class UIImageUpdater : MJFastImageLoader.MJFastImageLoaderNotification {
+	public class UIImageUpdater : MJFastImageLoader.MJFastImageLoaderNotification {
 		let imageView:UIImageView
+
 		init(imageView: UIImageView) {
 			self.imageView = imageView
 			super.init()
@@ -64,10 +137,132 @@ class ViewController: UIViewController {
 		override func notify(image: UIImage) {
 			print("notify")
 			DispatchQueue.main.async {
-				print("update")
+				print("update \(self.imageView.accessibilityHint)")
 				self.imageView.image = image
 			}
-			super.notify(image: image)
+		}
+	}
+
+	// MARK: Buttons
+
+	@IBAction func runAction(_ sender: Any) {
+		running = true
+		runButton.isEnabled = false
+		stepButton.isEnabled = false
+		stopButton.isEnabled = true
+		step()
+	}
+
+	@IBAction func stopAction(_ sender: Any) {
+		running = false
+		runButton.isEnabled = true
+		stepButton.isEnabled = true
+		stopButton.isEnabled = false
+	}
+
+	@IBAction func stepAction(_ sender: Any) {
+		runButton.isEnabled = true
+		stepButton.isEnabled = true
+		stopButton.isEnabled = false
+		step()
+	}
+
+	@IBAction func switchAction(_ sender: UISwitch) {
+		enabled = sender.isOn
+	}
+
+	@IBAction func changeThumbPx(_ sender: UITextField) {
+		if let value = sender.text {
+			MJFastImageLoader.shared.setThumbnailPx(pixels: Int(value)!)
+		}
+	}
+
+	// MARK: Test Execution
+
+	func step() {
+		// Clear the cache
+		MJFastImageLoader.shared.flush()
+		
+		// Blank out all images
+		imageViews.forEach({ (imageView) in
+			self.imageView.accessibilityHint = String( describing: imageViews.index(of: imageView) )
+			imageView.image = nil
+			imageView.backgroundColor = UIColor.red
+		})
+
+		// Put an image into each in rapid succesion, with goal of having a good user experience
+		testQueue.async {
+			self.imageViews.forEach({ (imgView) in
+				self.setNewImage(imgView: imgView)
+			})
+		}
+
+		// Start auto activity in three seconds
+		self.testQueue.asyncAfter(deadline: .now() + .seconds(3), execute: {
+			self.autoRefresh()
+		})
+	}
+
+	func autoRefresh() {
+		DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+			if ( 0 == self.imagesSet % 6 )
+			{
+				self.statusLabel.text = "Set \(self.imagesSet) Hit \(self.cacheHits) Waste \(MJFastImageLoader.wasteCount)"
+			}
+		})
+
+		if ( running ) {
+			// Select a random UIImageView and update it
+			let randomImageView = imageViews.shuffled().first!
+
+			// Blank it out
+			DispatchQueue.main.sync {
+				randomImageView.image = nil
+				randomImageView.backgroundColor = UIColor.orange
+			}
+
+			// Put a new image in
+			setNewImage(imgView: randomImageView)
+
+			// Do another one in 100ms
+			testQueue.asyncAfter(deadline: .now() + .milliseconds(100), execute: {
+				// Not infinite recursion, due to GCD.  Thank goodness.
+				self.autoRefresh()
+			})
+		}
+	}
+
+	var imagesSet = 0
+	var cacheHits = 0
+
+	func setNewImage( imgView: UIImageView ) {
+		imagesSet += 1
+		print("set image \(imageViews.index(of: imgView)) from index \(imageDataIndex % imageDatas.count)")
+		if let updater = imageUpdaters[imageViews.index(of: imgView)!] {
+			updater.cancel()
+		}
+		let data = imageDatas[imageDataIndex % imageDatas.count]
+		imageDataIndex += 1
+		if ( enabled )
+		{
+			_ = MJFastImageLoader.shared.enqueue(image: data, priority: .critical)
+			DispatchQueue.main.sync {
+				print("do set image \(imageViews.index(of: imgView)) from index \(imageDatas.index(of: data))")
+				let updater = UIImageUpdater(imageView: imgView)
+				imageUpdaters[imageViews.index(of: imgView)!] = updater
+				imgView.image = MJFastImageLoader.shared.image(image: data, notification: updater)
+				if ( nil != imgView.image )
+				{
+					print("non nil image")
+					cacheHits += 1
+				}
+			}
+		}
+		else
+		{
+			DispatchQueue.main.sync {
+				imgView.image = UIImage(data: data)
+			}
 		}
 	}
 
@@ -87,22 +282,44 @@ class ViewController: UIViewController {
 			guard let data = data, error == nil else { return }
 			print(response?.suggestedFilename ?? url.lastPathComponent)
 			print("Download Finished")
-			DispatchQueue.main.sync {
+			self.imageDatas.append(data)
+			/*DispatchQueue.main.sync {
 				self.imageView.backgroundColor = UIColor.black
 			}
 			MJFastImageLoader.shared.enqueue(image: data, priority: .critical)
 			DispatchQueue.main.sync {
 				self.imageView.image = MJFastImageLoader.shared.image(image: data, notification: UIImageUpdater(imageView: self.imageView))
-			}
+			}*/
 		}
 	}
 
 	func startTest() {
 		print("Begin of code")
-		if let url = URL(string: "https://picsum.photos/5000/3000") {
-			imageView.contentMode = .scaleAspectFit
+		statusLabel.text = "Downloading images..."
+		if let url = URL(string: "https://picsum.photos/5000/3000/?random") {
+			imageViews.forEach({ (imgView) in
+				imgView.contentMode = .scaleAspectFit
+			})
 			testQueue.async {
-				self.downloadImage(url: url)
+				let imageCount = 10
+				for i in 1...imageCount {
+					DispatchQueue.main.sync {
+						self.statusLabel.text = "Downloading image (\(i) of \(imageCount))..."
+					}
+					self.downloadImage(url: url)
+					while ( i > self.imageDatas.count )
+					{
+						// Normally a busy-wait would be undesirable, but for a demo to avoid smashing the source server this is good enough.
+						sleep(1)
+					}
+				}
+
+				DispatchQueue.main.sync {
+					self.statusLabel.text = "Ready"
+					self.runButton.isEnabled = true
+					self.stepButton.isEnabled = true
+					self.stopButton.isEnabled = false
+				}
 			}
 		}
 		print("End of code. The image will continue downloading in the background and it will be loaded when it ends.")
