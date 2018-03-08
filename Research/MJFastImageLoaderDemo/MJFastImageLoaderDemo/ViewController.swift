ba460e1739ec08ce8e15c098b98d3eba9e157b7c
Mark Jerde <mark.a.jerde@gmail.com>
2018-03-08 10:04:14 -0600

    Revert "Memory consumption notes, which must be wrong.  Perhaps something about GCD hides memory consumption from mach_task_basic_info and from Xcode's Debug Navigator."
    This reverts commit 36c1c3ff26bc929f2a8124038012b20e44414f9d.

--- Research/MJFastImageLoaderDemo/MJFastImageLoaderDemo/ViewController.swift
+++ Research/MJFastImageLoaderDemo/MJFastImageLoaderDemo/ViewController.swift
@@ -115,32 +115,7 @@ class ViewController: UIViewController {
 
 		FastImageLoader.shared.setCriticalProcessingConcurrencyLimit(limit: ProcessInfo.processInfo.activeProcessorCount)
 
-		reportMemoryTimer = Timer.scheduledTimer(timeInterval: 0.1,
-												 target: self,
-												 selector: #selector(self.reportMemory),
-												 userInfo: nil,
-												 repeats: true)
-
 		self.startTest()
-	}
-
-	var reportMemoryTimer = Timer()
-	@objc func reportMemory() {
-		var info = mach_task_basic_info()
-		var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size)/4
-		
-		let kerr: kern_return_t = withUnsafeMutablePointer(to: &info) {
-			$0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-				task_info(mach_task_self_,
-						  task_flavor_t(MACH_TASK_BASIC_INFO),
-						  $0,
-						  &count)
-			}
-		}
-
-		if kerr == KERN_SUCCESS {
-			NSLog("MEM_REPORT \(info.resident_size) bytes")
-		}
 	}
 
 	//Calls this function when the tap is recognized.
