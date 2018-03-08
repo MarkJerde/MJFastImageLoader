456bd5cb68f56fd13dda829396f4b56cecb448ae
Mark Jerde <mark.a.jerde@gmail.com>
2018-03-08 15:52:42 -0600

    It was observed that Data.hashValue is frequently the same value.  As a result the secondary hash was switched to manual examination of data and a tertiary hash was added.  Number of bytes included in the hash was also limited for sake of performance.

--- MJFastImageLoader/MJFastImageLoader/DataIdentity.swift
+++ MJFastImageLoader/MJFastImageLoader/DataIdentity.swift
@@ -34,8 +34,11 @@ class DataIdentity : Equatable, Hashable {
 	// Uses the hash value of the original object, which has a risk of collision so it
 	// stores a secondary hash of a subset of the original content providing 1-in-n-squared
 	// probability of a incorrect equality.
+	// It was observed that it is common for Data objects of equal size to have the same
+	// hash value, so a tertiary hash is also performed.
 	private(set) var hashValue: Int
-	private var partialHash: Int
+	private(set) var partialHash1: Int
+	private(set) var partialHash2: Int
 
 	/// Creates an identity item object describing the provided Data.
 	///
@@ -44,13 +47,50 @@ class DataIdentity : Equatable, Hashable {
 		hashValue = data.hashValue
 
 		// If the data is small, use all except the first byte for the secondary hash.  Otherwise use a middle-ish one tenth of the data.
-		let short = data.count < 1000
-		let partialStart = short ? 1 : (data.count / 10 * 5)
-		let partialEnd = short ? data.count : (data.count / 10 * 6)
-		partialHash = data.subdata(in: Range<Data.Index>(partialStart...partialEnd)).hashValue
+		let maximumSampleSize = 3200
+		let short = data.count < maximumSampleSize
+		let partial1Start = short ? 1 : (data.count / 100 * 50)
+		var partial1End = short ? data.count : (data.count / 100 * 51)
+		if ( partial1End - partial1Start > maximumSampleSize ) {
+			partial1End = partial1Start + maximumSampleSize - 1
+		}
+		partialHash1 = 0
+		// Iterate over the bytes since we already know that .hashValue doesn't do unique so well.
+		for i in partial1Start...partial1End {
+			// Is there a better way to do this?  Unsafe pointers may provide a good solution that works in larger chunks.
+			//let was = partialHash1
+			var part = data[i]
+			if ( 0 == i % 3 ) {
+				// Salt every third with the index.  This because repeating patterns can cause the hash to self-clear otherwise.
+				part ^= UInt8(i % 0xFF)
+			}
+			let shifter = ((i % 8) * 8)
+			partialHash1 ^= Int(part) << shifter
+		}
+
+		// If the data is small, use all except the first two bytes for the secondary hash.  Otherwise use a early-ish one tenth of the data.
+		let partial2Start = short ? 1 : (data.count / 100 * 20)
+		var partial2End = short ? data.count : (data.count / 100 * 21)
+		if ( partial2End - partial2Start > maximumSampleSize ) {
+			partial2End = partial2Start + maximumSampleSize - 1
+		}
+		partialHash2 = 0
+		// Iterate over the bytes since we already know that .hashValue doesn't do unique so well.
+		for i in partial2Start...partial2End {
+			// Is there a better way to do this?  Unsafe pointers may provide a good solution that works in larger chunks.
+			var part = data[i]
+			if ( 0 == (i + 1) % 3 ) { // Plus one here to un-align with partialHash1
+				// Salt every third with the index.  This because repeating patterns can cause the hash to self-clear otherwise.
+				part ^= UInt8(i % 0xFF)
+			}
+			let shifter = ((i % 8) * 8)
+			partialHash2 ^= Int(part) << shifter
+		}
 	}
 
 	static func ==(lhs: DataIdentity, rhs: DataIdentity) -> Bool {
-		return lhs.hashValue == rhs.hashValue && lhs.partialHash == rhs.partialHash
+		return lhs.hashValue == rhs.hashValue
+			&& lhs.partialHash1 == rhs.partialHash1
+			&& lhs.partialHash2 == rhs.partialHash2
 	}
 }
